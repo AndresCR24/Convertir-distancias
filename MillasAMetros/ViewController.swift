4936e531b8e1958cd2a3e778de6719686c20217b
AndresCR24 <c-andres-98@hotmail.com>
2024-03-06 19:46:57 -0500

    Save the mini project; practice UIKit

--- MillasAMetros/ViewController.swift
+++ MillasAMetros/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var distanceTextField: UITextField!
+    @IBOutlet var segmentedControl: UISegmentedControl!
+    @IBOutlet var resultLabel: UILabel!
+    
+    let mileUnit: Double = 1.609
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        resultLabel.text = ""
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func convertButton(_ sender: Any) {
+        let selectedIndex = segmentedControl.selectedSegmentIndex
+        let texFieldVal = Double(distanceTextField.text!)!
+        
+        print("Hemos seleccionado \(selectedIndex)")
+        
+        if selectedIndex == 0 {
+            //let convertedValue = ((texFieldVal / mileUnit) * 1000).rounded() / 1000
+            let convertedValue = texFieldVal / mileUnit
+            let roundValue = String(format: "%.3f", convertedValue)
+            
+            resultLabel.text = "El resultado en millas es: \(roundValue)"
+        } else {
+            let convertedValue = texFieldVal * mileUnit
+            let roundValueKm = String(format: "%.3f", convertedValue)
+            resultLabel.text = "El resultado en kilometros es: \(roundValueKm)"
+        }
+    }
+    
+    
 }
 
